9dd1ef9a9d04cc4ada613939b61b93e1bb040d2f
martinzwm <martinma@g.harvard.edu>
2024-04-25 11:16:40 -0400

    Fix attention dimension.

--- transformer.py
+++ transformer.py
@@ -8,13 +8,8 @@ import torch.nn.functional as F
 class TransformerBlock(nn.Module):
     def __init__(self, d_model, d_linear, num_heads, dropout):
         super(TransformerBlock, self).__init__()
-        # Query, Key, Value projection matrices
-        self.Wq = nn.Linear(d_model, d_model)
-        self.Wk = nn.Linear(d_model, d_model)
-        self.Wv = nn.Linear(d_model, d_model)
-
         # Multihead attention
-        self.multihead_attn = nn.MultiheadAttention(d_model, num_heads)
+        self.multihead_attn = nn.MultiheadAttention(d_model, num_heads, batch_first=True)
 
         # Pointwise feedforward
         self.linear1 = nn.Linear(d_model, d_linear)
@@ -31,13 +26,8 @@ class TransformerBlock(nn.Module):
             - key_padding_mask [=] (N, T), used to prevent attention on special 
                 tokens (e.g., <PAD>, <SEP>). A value of 1 means ignored, 0 means keep.
         """
-        # Query, Key, Value projection
-        q = self.Wq(x)
-        k = self.Wk(x)
-        v = self.Wv(x)
-
         # Attention
-        attn_output, _ = self.multihead_attn(q, k, v, mask)
+        attn_output, alpha = self.multihead_attn(x, x, x, mask)
         x = x + self.dropout(attn_output) # dropout and residual connection
         x = self.norm1(x)
         
@@ -50,23 +40,30 @@ class TransformerBlock(nn.Module):
 
 """Code for stacking multiple transformer block together"""
 class Transformer(nn.Module):
-    def __init__(self, d_input, d_model, d_linear, num_heads, dropout, num_layers):
+    def __init__(self, d_input, d_model, d_linear, num_heads, dropout, num_layers, max_T=100):
         super(Transformer, self).__init__()
+        # Define transformer layers
+        self.layers = nn.ModuleList(
+            [TransformerBlock(d_model, d_linear, num_heads, dropout) for _ in range(num_layers)]
+        )
 
         # Define non-transformer layers
         self.cls_token = nn.Parameter(torch.randn(1, 1, d_model))
-        self.input_linear = nn.Linear(d_input, d_model)
-
-        # First transformer layer
-        layers = [TransformerBlock(d_model, d_linear, num_heads, dropout)]
-
-        # Rest of the transformer layers
-        layers += [TransformerBlock(d_model, d_linear, num_heads, dropout) for _ in range(num_layers-1)]
-        self.layers = nn.ModuleList(layers)
+        self.input_linear = nn.Sequential(
+            nn.Linear(d_input, d_model),
+            nn.ReLU()
+        )
+        self.pos_E = nn.Embedding(max_T, d_model)
+        self.head = nn.Linear(d_model, 2)
 
     def forward(self, x, mask=None):
-        # Add class token and linear projection
+        # Apply linear projection and add position embedding
         x = self.input_linear(x)
+        B, T = x.shape[:2]
+        pos_ids = torch.arange(T).expand(B, -1).to(x.device)
+        x  = x + self.pos_E(pos_ids)
+
+        # Concatenate [CLS] token
         cls_tokens = self.cls_token.expand(x.shape[0], -1, -1)
         x = torch.cat([cls_tokens, x], dim=1)
 
@@ -74,6 +71,9 @@ class Transformer(nn.Module):
         for layer in self.layers:
             x = layer(x)
         return x
+
+    def get_param(self, x):
+        return self.head(x)
 
 
 class TestModel:
@@ -101,12 +101,11 @@ class TestModel:
         dropout = 0.1
         num_layers = 6
         transformer = Transformer(d_input, d_model, d_linear, num_heads, dropout, num_layers)
-        pdb.set_trace()
         
         # Define input
         input = torch.randn(16, 10, d_input)
         output = transformer(input)
-        print(output.shape)
+        print(output.shape) # should be (16, 11, 64)
 
 
 if __name__ == "__main__":
