d31da963d5347244315d1c76cbf2b015fca00be5
martinzwm <martinma@g.harvard.edu>
2024-04-25 18:02:08 -0400

    Add transformer decoder architecture.

--- transformer.py
+++ transformer.py
@@ -18,7 +18,7 @@ class TransformerBlock(nn.Module):
         self.norm2 = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x, mask=None, key_padding_mask=None):
+    def forward(self, x, mask=None):
         """
         Args:
             - x [=] (B, T, d)
@@ -27,7 +27,7 @@ class TransformerBlock(nn.Module):
                 tokens (e.g., <PAD>, <SEP>). A value of 1 means ignored, 0 means keep.
         """
         # Attention
-        attn_output, alpha = self.multihead_attn(x, x, x, mask)
+        attn_output, alpha = self.multihead_attn(x, x, x, attn_mask=mask)
         x = x + self.dropout(attn_output) # dropout and residual connection
         x = self.norm1(x)
         
@@ -56,7 +56,7 @@ class Transformer(nn.Module):
         self.pos_E = nn.Embedding(max_T, d_model)
         self.head = nn.Linear(d_model, 2)
 
-    def forward(self, x, mask=None):
+    def forward(self, x):
         # Apply linear projection and add position embedding
         x = self.input_linear(x)
         B, T = x.shape[:2]
@@ -76,8 +76,45 @@ class Transformer(nn.Module):
         return self.head(x)
 
 
+class TransformerDecoder(Transformer):
+
+    def __init__(self, d_input, d_model, d_linear, num_heads, dropout, num_layers, max_T=100):
+        super(TransformerDecoder, self).__init__(d_input, d_model, d_linear, num_heads, dropout, num_layers, max_T)
+        self.output_linear = nn.Linear(d_model, d_input)
+
+    def forward(self, x):
+        # Apply linear projection and add position embedding
+        x = self.input_linear(x)
+        B, T = x.shape[:2]
+        pos_ids = torch.arange(T).expand(B, -1).to(x.device)
+        x  = x + self.pos_E(pos_ids)
+
+        # Concatenate [CLS] token
+        cls_tokens = self.cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat([cls_tokens, x], dim=1)
+
+        # Create masking
+        mask = torch.triu(torch.ones(T+1, T+1)).to(x.device)
+
+        # Apply transformer layers
+        for layer in self.layers:
+            x = layer(x, mask)
+        return x
+    
+    def pred_next_step(self, emb):
+        return self.output_linear(emb)
+    
+    def generate(self, x, gen_T):
+        with torch.no_grad():
+            for i in range(gen_T):
+                emb = self.forward(x)
+                x_out = self.pred_next_step(x_out)
+                x = torch.cat([x, x_out], dim=1)
+        return x
+
+
 class TestModel:
-     
+
     def test_transformer_block(self):
         # Define Transformer
         d_model = 64
@@ -92,7 +129,7 @@ class TestModel:
         print(output.shape)
 
 
-    def test_transformer(self):
+    def test_transformer_encoder(self):
         # Define Transformer
         d_input = 4
         d_model = 64
@@ -107,8 +144,25 @@ class TestModel:
         output = transformer(input)
         print(output.shape) # should be (16, 11, 64)
 
+    
+    def test_transformer_decoder(self):
+         # Define Transformer
+        d_input = 4
+        d_model = 64
+        d_linear = 32
+        num_heads = 8
+        dropout = 0.1
+        num_layers = 6
+        transformer = TransformerDecoder(d_input, d_model, d_linear, num_heads, dropout, num_layers)
+        
+        # Define input
+        input = torch.randn(16, 10, d_input)
+        output = transformer(input)
+        print(output.shape) # should be (16, 11, 64)
+
 
 if __name__ == "__main__":
     test_model = TestModel()
     # test_model.test_transformer_block()
-    test_model.test_transformer()
+    # test_model.test_transformer_encoder()
+    test_model.test_transformer_decoder()
