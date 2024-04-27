e107a06e40aea3a184e450b2776b096636823a39
martinzwm <martinma@g.harvard.edu>
2024-04-27 17:02:21 -0400

    Separate loss functions to allow training with both losses.

--- train_transformer.py
+++ train_transformer.py
@@ -1,4 +1,5 @@
 import pdb
+import json
 import tqdm
 import numpy as np
 import wandb
@@ -9,24 +10,21 @@ import torch.nn.functional as F
 import torch.optim as optim
 from info_nce import InfoNCE
 
-from transformer import Transformer
+from transformer import Transformer, TransformerDecoder
 from dynamics import get_dataloader
 from eval_transformer import evaluate_transformer
 
 
 def train_transformer(config=None):
-    log_to_wandb = False
-
     # Determine if we are doing hyperparameter search
     if config is None:
         wandb.init(config=config, project="contr_transformer", entity="contrastive-time-series")
         config = wandb.config
-        log_to_wandb = True
     
+    log_to_wandb = config['log_to_wandb']
     # Determine if we are loggin
-    if 'log_to_wandb' in config and config['log_to_wandb']:
+    if log_to_wandb:
         wandb.init(config=config, project="contr_transformer", entity="contrastive-time-series")
-        log_to_wandb = True
         
     # Hyperparameter
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -39,16 +37,22 @@ def train_transformer(config=None):
     num_heads = config['num_heads']
     dropout = config['dropout']
     num_layers = config['num_layers']
+    mode = config['mode']
     
     # Load model
-    encoder = Transformer(d_input, d_model, d_linear, num_heads, dropout, num_layers).to(device)
-    encoder.train()
-    # encoder.load_state_dict(torch.load("./ckpts/framework1_best_2000.pt"))
-    optimizer = optim.Adam(encoder.parameters(), lr=lr)
+    if mode == 'encoder':
+        model = Transformer(d_input, d_model, d_linear, num_heads, dropout, num_layers).to(device)
+    elif mode == 'decoder' or mode == 'decoder-contrastive':
+        model = TransformerDecoder(d_input, d_model, d_linear, num_heads, dropout, num_layers).to(device)
+    else:
+        raise ValueError("Invalid mode")
+    
+    model.train()
+    # model.load_state_dict(torch.load("./ckpts/framework1_best_2000.pt"))
+    optimizer = optim.Adam(model.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=num_epochs, T_mult=1, eta_min=1e-4)
 
     dataloader = get_dataloader(batch_size=32, data_path="./data/train_data.pickle", num_workers=8)
-    loss_fn_contrastive = InfoNCE()
 
     for epoch in tqdm.tqdm(range(num_epochs)):
         total_loss = 0.0
@@ -67,23 +71,22 @@ def train_transformer(config=None):
 
             # Forward pass
             data = data.view(-1, time_size, state_size)
-            embedding = encoder(data)
-
-            # Contrastive loss
-            embedding = embedding[:, 0, :]
-            embedding = embedding.view(batch_size, 2, -1)
-
-            # Compute loss - contrastive
-            sample1 = embedding[:, 0, :]
-            sample2 = embedding[:, 1, :]
-            sample1 = sample1 / torch.norm(sample1, dim=1, keepdim=True)
-            sample2 = sample2 / torch.norm(sample2, dim=1, keepdim=True)
-            loss = loss_fn_contrastive(sample1, sample2)
-
-            # # Compute loss - supervised
-            # W = W.to(device)
-            # pred_W = encoder.get_param(embedding)
-            # loss = F.mse_loss(pred_W[:, 0, :], W[:, 2:]) + F.mse_loss(pred_W[:, 1, :], W[:, 2:])
+            emb = model(data)
+            
+            if mode == "encoder": # Compute contrastive loss
+                loss = contrastive_loss(emb, batch_size)
+            
+            elif mode == "decoder": # Compute regression loss
+                # Get predicted next steps
+                emb = emb[:, 1:-1, :]
+                out = model.pred_next_step(emb)
+                # Compute regression loss
+                loss = regression_loss(out, data)
+            
+            elif mode == "decoder-contrastive": # Compute contrastive loss + regression loss
+                emb = emb[:, 1:-1, :]
+                out = model.pred_next_step(emb)
+                loss = contrastive_loss(emb, batch_size) + regression_loss(out, data)
 
             # Backward pass
             loss.backward()
@@ -104,9 +107,9 @@ def train_transformer(config=None):
         # Save model
         if (epoch+1) % 100 == 0:
             model_path = f"./ckpts/model_{epoch+1}.pt"
-            torch.save(encoder.state_dict(), model_path)
+            torch.save(model.state_dict(), model_path)
             train_mae, val_mae = evaluate_transformer(
-                encoder, ckpt_path=model_path
+                model, ckpt_path=model_path
             )
             print("MAE (params) on the training set: ", train_mae.mean())
             if log_to_wandb:
@@ -114,25 +117,55 @@ def train_transformer(config=None):
                 wandb.log({"train_mae": train_mae.mean().item()})
 
 
+def contrastive_loss(emb, batch_size):
+    """Compute contrastive loss for a batch of embeddings from transformer encoder."""
+    # Define loss function
+    loss_fn = InfoNCE()
+    
+    # Separate positive and negative samples
+    emb = emb[:, 0, :]
+    emb = emb.view(batch_size, 2, -1)
+    sample1 = emb[:, 0, :]
+    sample2 = emb[:, 1, :]
+    
+    # Compute contrastive loss
+    sample1 = sample1 / torch.norm(sample1, dim=1, keepdim=True)
+    sample2 = sample2 / torch.norm(sample2, dim=1, keepdim=True)
+    loss = loss_fn(sample1, sample2)
+    return loss
+
+
+def regression_loss(out, data):
+    """Compute regression loss for a batch of embeddings from transformer decoder."""
+    # Define loss function
+    loss_fn = nn.MSELoss()
+    # Get gt next steps
+    y = data[:, 1:, :]
+    # Compute regression loss
+    loss = loss_fn(out, y)
+    return loss
+
+
 def transformer_hyperparam_search():
     sweep_config = {
         'method': 'random', 
         'metric': {'name': 'loss', 'goal': 'minimize'},
         'parameters': {
-            # 'learning_rate': {'distribution': 'log_uniform', 'min': int(np.floor(np.log(1e-4))), 'max': int(np.floor(np.log(1e-1)))},
             'learning_rate': {
                 'distribution': 'log_uniform',
                 'min': np.log(1e-4),
-                'max': np.log(1e-1)
+                'max': np.log(1e-2)
             },
             'num_epochs': {'values': [3000]},
             'batch_size': {'values': [32]},
             'd_input': {'values': [4]},
-            'd_model': {'values': [16, 32, 64, 128, 256]},
+            'd_model': {'values': [32, 64, 128, 256]},
             'd_linear': {'values': [16, 32, 64, 128, 256]},
             'num_heads': {'values': [4, 8]},
             'dropout': {'values': [0.1, 0.2, 0.4]},
             'num_layers': {'values': [2, 6, 10]},
+            'mode': {'values': ['decoder-contrastive']},
+            'log_to_wandb': {'values': [True]}
         }
     }
 
@@ -142,19 +175,26 @@ def transformer_hyperparam_search():
 
 if __name__ == "__main__":
     # # Train single run
-    # default_config = {
-    #     'learning_rate': 1e-3,
-    #     'num_epochs': 5000,
-    #     'batch_size': 32,
-    #     'd_input': 4,
-    #     'd_model': 32,
-    #     'd_linear': 64,
-    #     'num_heads': 4,
-    #     'dropout': 0.2,
-    #     'num_layers': 6,
-    #     'log_to_wandb': True,
-    # }
-    # train_transformer(default_config)
+    # Define hyperparameters
+    default_config = {
+        'learning_rate': 0.00013,
+        'num_epochs': 5000,
+        'batch_size': 32,
+        'd_input': 4,
+        'd_model': 256,
+        'd_linear': 64,
+        'num_heads': 4,
+        'dropout': 0.2,
+        'num_layers': 4,
+        'log_to_wandb': False,
+        'mode': 'decoder-contrastive'
+    }
+    # # Or load from config file
+    # config_file = "./configs/transformer_decoder_config.json"
+    # with open(config_file, "r") as f:
+    #     default_config = json.load(f)
 
-    # Hyperparameter search
-    transformer_hyperparam_search()
+    train_transformer(default_config)
+
+    # # Hyperparameter search
+    # transformer_hyperparam_search()
